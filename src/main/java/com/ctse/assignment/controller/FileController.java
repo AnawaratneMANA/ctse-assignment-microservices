934e3a59aa412a309e6e2cc3ec15bde1991cf1d7
AnawaratneMANA <it19086408@my.sliit.lk>
2022-05-03 20:59:49 +0530

    COMMIT: File uploading done to Azure blob.

--- src/main/java/com/ctse/assignment/controller/FileController.java
+++ src/main/java/com/ctse/assignment/controller/FileController.java
@@ -1,8 +1,11 @@
 package com.ctse.assignment.controller;
 
+import com.ctse.assignment.service.BlobFileUploadService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,7 +17,12 @@ import java.util.UUID;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/file")
 public class FileController {
+
+    private final BlobFileUploadService blobFileUploadService;
+
     @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("/upload")
     public String uploadFile(MultipartFile file){
@@ -22,15 +30,20 @@ public class FileController {
         log.info("Size: " + file.getSize());
         log.info("Content Type : " + file.getContentType());
         String destinationFilename = "./uploads/" + file.getOriginalFilename() + UUID.randomUUID();
+        // Cache the file locally.
+        String response;
         try {
             Files.copy(
                     file.getInputStream(),
                     Path.of(destinationFilename),
                     StandardCopyOption.REPLACE_EXISTING
             );
+
+            // Upload the file to the server.
+            response = blobFileUploadService.storeFile(file.getOriginalFilename(),file.getInputStream(), file.getSize());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return destinationFilename + "Has been saved!";
+        return  destinationFilename + "Has been saved!" + " and " + response;
     }
 }
