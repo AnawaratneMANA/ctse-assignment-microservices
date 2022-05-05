c9185c2a9a170f0464063372a27c270c784fd9c6
AnawaratneMANA <it19086408@my.sliit.lk>
2022-05-05 08:39:32 +0530

    UPDATE: Maven file updated with docker build commands

--- src/main/java/com/ctse/assignment/FileUploadServiceApplication.java
+++ src/main/java/com/ctse/assignment/FileUploadServiceApplication.java
@@ -1,5 +1,4 @@
 package com.ctse.assignment;
-
 import com.ctse.assignment.util.AzureBlobProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
